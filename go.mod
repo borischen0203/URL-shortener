c52bf7270831400b0d675ec574c5d6a045bd5e40
Boris Chen <borischen0203@gmail.com>
2021-07-22 00:45:59 +0800

    UPD. update test file

--- go.mod
+++ go.mod
@@ -8,5 +8,7 @@ require (
 	github.com/gookit/validate v1.2.11 // indirect
 	github.com/nsf/gocode v0.0.0-20190302080247-5bee97b48836 // indirect
 	github.com/speps/go-hashids v2.0.0+incompatible // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
 	go.mongodb.org/mongo-driver v1.5.4 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
---
+++ test/urlHandler_test.go
@@ -0,0 +1,44 @@
+package test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"encoding/json"
+	"url-shortener/database"
+	"url-shortener/dto"
+	"url-shortener/router"
+
+	"github.com/stretchr/testify/assert"
+	// "github.com/stretchr/testify/assert"
+	// "github.com/tedmax100/gin-angular/router"
+)
+
+var GenerateUrl = "/api/url-shortener/v1/url"
+
+func TestGenerateUrl(t *testing.T) {
+	database.Setup()
+	router := router.SetupRouter()
+	w := httptest.NewRecorder()
+
+	requestBody := dto.UrlShortenerRequest{
+		LongUrl: "https://www.youtube.com/",
+		Alias:   "",
+	}
+	responseBody := dto.UrlResponse{
+		LongUrl:  "https://www.youtube.com/",
+		ShortUrl: "http://localhost:8080/gJXz8NqV7N40l",
+	}
+
+	request, _ := json.Marshal(requestBody)
+	expectedBody, _ := json.Marshal(responseBody)
+
+	req, _ := http.NewRequest(http.MethodPost, GenerateUrl, strings.NewReader(string(request)))
+
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, string(expectedBody), w.Body.String())
+}
